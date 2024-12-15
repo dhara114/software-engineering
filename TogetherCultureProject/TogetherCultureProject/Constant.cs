4df42aa725c50e95a5c42300794b005a44656dc4
IP311 <IP311@student.aru.ac.uk>
2024-12-15 21:42:14 +0000

    Constant class and data entry validation

--- TogetherCultureProject/TogetherCultureProject/Constant.cs
+++ TogetherCultureProject/TogetherCultureProject/Constant.cs
@@ -8,5 +8,10 @@ namespace TogetherCultureProject
 {
     class Constant
     {
+        //selects all data in User table
+        public static string SELECTALLUser = "SELECT * FROM [User]";
+        //inserts into all columns of a row in User table
+        public static string INSERTALLUser = "INSERT INTO [User] (FirstName, LastName, PhoneNumber, Email, Address, TownCity, Postcode, Password)" +
+                "VALUES (@FirstName, @LastName, @PhoneNumber, @Email, @Address, @TownCity, @Postcode, @Password)";
     }
 }
--- TogetherCultureProject/TogetherCultureProject/Form1.cs
+++ TogetherCultureProject/TogetherCultureProject/Form1.cs
@@ -55,21 +55,41 @@ namespace LoginAndRegistration
 
         private void RegButton_Click(object sender, EventArgs e)
         {
-            DBConnection dbConn = DBConnection.getInstanceOfDBConnection();
-            //DataSet datasetUser = dbConn.getDataSet("SELECT * FROM User");
+            DBConnection dbConn = DBConnection.getInstanceOfDBConnection(); //connect to db
+            DataSet datasetUser = dbConn.getDataSet(Constant.SELECTALLUser);
 
+            //variables holding text box values
             string FirstName = FirstNameText.Text;
             string LastName = LastNameText.Text;
-            int PhoneNumber = Convert.ToInt32(PhoneNumText.Text);
+            string PhoneNumberInitial = (PhoneNumText.Text); //phonenum stored here initially to check for non-number values
+            int PhoneNumber;
             string Email = EmailText.Text;
             string Address = AddressText.Text;
             string TownCity = TownCityText.Text;
             string Postcode = PostcodeText.Text;
             string Password = PasswordText.Text;
             string ConfirmPassword = ConfirmPasswordText.Text;
-            dbConn.saveToDB("INSERT INTO [User] (FirstName, LastName, PhoneNumber, Email, Address, TownCity, Postcode, Password)" +
-                "VALUES (@FirstName, @LastName, @PhoneNumber, @Email, @Address, @TownCity, @Postcode, @Password)",
-                FirstName, LastName, PhoneNumber, Email, Address, TownCity, Postcode, Password);
+            if (FirstName.Any(Char.IsDigit) || LastName.Any(Char.IsDigit)) //checks for numbers in name variables
+            {
+                MessageBox.Show("Name field(s) contain an invalid value, numbers cannot be used");
+            }
+            else if (PhoneNumberInitial.All(Char.IsDigit)) //checks if entire string is numbers
+            {
+                PhoneNumber = Convert.ToInt32(PhoneNumberInitial); //phonenum string passed to int variable
+                if (Password != ConfirmPassword) //password confirmation validations
+                {
+                    MessageBox.Show("Passwords do not match, enter matching passwords in both password fields");
+                }
+                else
+                {
+                    //insert text box values into db
+                    dbConn.saveToDB(Constant.INSERTALLUser, FirstName, LastName, PhoneNumber, Email, Address, TownCity, Postcode, Password);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Phone number field contains an invalid value, only numbers must be entered in the phone number field");
+            }
         } 
 
         private void ShowPasswordChkbx_CheckedChanged(object sender, EventArgs e)
